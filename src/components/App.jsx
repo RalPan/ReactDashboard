17d46a2885ac24cf70cdf9b8ae87e4765d2c8b14
raluc <raluca1pantilie@gmail.com>
2023-09-22 15:47:06 +0100

    Home and review pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,10 @@ import "./App.css";
 import Layout from "./layout/Layout";
 import Header from "./layout/Header";
 import { UserProvider } from "../client/context/UserContext";
-
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import ErrorBoundary from "./base/ErrorBoundary";
+import Reviews from "./pages/Reviews";
 
 function App() {
   return (
@@ -10,6 +13,19 @@ function App() {
     <UserProvider>
       <Layout>
       <Header />
+      <Routes>
+        <Route path="/" element={<ErrorBoundary>
+                                     <Home />
+                               </ErrorBoundary> }/>
+
+          <Route path="/reviews"
+                        element={
+                          <ErrorBoundary>
+                            <Reviews />
+                          </ErrorBoundary>
+                          } />
+          {/* <Route path="/sales" element={} /> */}
+        </Routes>
       </Layout>
       </UserProvider>
     </>
